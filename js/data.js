85b48cce3330dff382cf10de9f28990336eb78b2
Hame313 <beganhame@gmail.com>
2021-03-24 13:43:03 +0100

    fullcommit

---
+++ js/data.js
@@ -0,0 +1,234 @@
+var data = [
+    {
+    id: 'Jan',
+    date: '13th January 2021',
+    name: 'Lorem',
+    color: '#e5e5dc',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum..'
+  },
+  {
+    id: 'Jan',
+    date: '15th January 2021',
+    name: 'Lorem',
+    color: '#e5e5dc',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum..'
+  },
+  {
+    id: 'Jan',
+    date: '26th January 2021',
+    name: 'Lorem',
+    color: '#e5e5dc',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.'
+  },
+  {
+    id: 'Feb',
+    date: '16th February 2021',
+    name: 'Lorem',
+    color: '#e5e5dc',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniamm..'
+  },
+  {
+    id: 'Feb',
+    date: '21st February 2021',
+    name: 'Lorem',
+    color: '#e5e5dc',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.'
+  },
+  {
+    id: 'Mar',
+    date: '10th March 2021',
+    name: 'Lorem',
+    color: '#e5e5dc',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam'
+  },
+  {
+    id: 'Mar',
+    date: '25th March 2021',
+    name: 'Lorem',
+    color: '#e5e5dc',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.'
+  },
+  {
+    id: 'Apr',
+    date: '2nd April 2021',
+    name: 'Lorem',
+    color: '#e5e5dc',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam'
+  },
+  {
+    id: 'Apr',
+    date: '6th April 2021',
+    name: 'Mahavir Jayanti',
+    color: '#e5e5dc',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam'
+  },
+  {
+    id: 'Apr',
+    date: '10th April 2021',
+    name: 'Good Friday',
+    color: '#e5e5dc',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam'
+  },
+  {
+    id: 'Apr',
+    date: '14th April 2021',
+    name: 'Lorem',
+    color: '#e5e5dc',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam'
+  },
+  {
+    id: 'May',
+    date: '7th May 2021',
+    name: 'Lorem',
+    color: '#e5e5dc',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.'
+  },
+  {
+    id: 'May',
+    date: '25th May 2021',
+    name: 'Lorem',
+    color: '#e5e5dc',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.'
+  },
+  {
+    id: 'Jul',
+    date: '31st July 2021',
+    name: 'Lorem',
+    color: '#e5e5dc',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam".'
+  },
+  {
+    id: 'Aug',
+    date: '3rd August 2021',
+    name: 'Rakhi',
+    color: '#e5e5dc',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.'
+  },
+  {
+    id: 'Aug',
+    date: '11th August 2021',
+    name: 'lorem',
+    color: '#e5e5dc',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam'
+  },
+  {
+    id: 'Aug',
+    date: '15th August 2021',
+    name: 'Independence Day',
+    color: '#e5e5dc',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam'
+  },
+  {
+    id: 'Aug',
+    date: '22nd August 2021',
+    name: 'lorem',
+    color: '#e5e5dc',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam'
+  },
+  {
+    id: 'Aug',
+    date: '29th August 2021',
+    name: 'lorem',
+    color: '#e5e5dc',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam'
+  },
+  {
+    id: 'Aug',
+    date: '31st August 2021',
+    name: 'lorem',
+    color: '#e5e5dc',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam'
+  },
+  {
+    id: 'Oct',
+    date: '2nd October 2021',
+    name: 'lorem',
+    color: '#e5e5dc',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam'
+  },
+  {
+    id: 'Oct',
+    date: '25th October 2021',
+    name: 'lorem',
+    color: '#e5e5dc',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.'
+  },
+  {
+    id: 'Nov',
+    date: '14th November 2021',
+    name: 'lorem',
+    color: '#e5e5dc',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam'
+  },
+  {
+    id: 'Nov',
+    date: '16th November 2021',
+    name: 'lorem',
+    color: '#e5e5dc',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam'
+  },
+  {
+    id: 'Nov',
+    date: '20th November 2021',
+    name: 'lorem',
+    color: '#e5e5dc',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam'
+  },
+  {
+    id: 'Nov',
+    date: '30th November 2021',
+    name: 'lorem',
+    color: '#e5e5dc',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam. '
+  },
+  {
+    id: 'Dec',
+    date: '25th December 2021',
+    name: 'Lorem',
+    color: '#e5e5dc',
+    description: 'Write the description here.'
+  }
+];
+// alert(data.length)
+let len = data.length;
+len-=1;
+for (var i=len; i>=0; i--){
+  
+    // alert(data[i].date);
+    // cardTitle[i].innerHTML = data[i].name;
+    $("section#events").prepend(
+      `<div class="wrapper" id="${data[i].id}">
+          <h3 class="upper">Your project or whatever</h3>
+          <a href="#" class="meetup">${data[i].name}</a>
+          <p class="group">${data[i].description}</p>
+          <p class="details">
+            <span class="row">
+              <i class="material-icons md-36 icon">event</i>
+              <span class="row-item">
+              <time>${data[i].date}</time>
+              </span>
+            </span>
+          </p>
+      </div>`
+    )
+  }
+
+  for (var i=len+1; i>0; i--){
+    $(".wrapper:nth-child("+i+")").css({
+      background: data[i-1].color
+    })
+  }
+
+  
+const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun","Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+var date = new Date();
+var monName = monthNames[date.getMonth()];
+
+// alert(monName);
+
+$(".months span").click(function(){
+  $(".months span").removeClass("currentMon")
+    $(this).closest("span").toggleClass("currentMon")
+})
+
